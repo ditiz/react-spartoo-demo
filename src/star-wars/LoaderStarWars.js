671660476e563c33b165c88e06e6a8acd88c0307
Mathieu <mathieu.25565@gmail.com>
2019-01-06 19:50:28 +0100

    add loader

--- src/star-wars/LoaderStarWars.js
+++ src/star-wars/LoaderStarWars.js
@@ -4,6 +4,8 @@ import loader1 from '../loader/loader_star-wars1.gif';
 import loader2 from '../loader/loader_star-wars2.gif';
 import loader3 from '../loader/loader_star-wars3.gif';
 import loader4 from '../loader/loader_star-wars4.gif';
+import loader5 from '../loader/loader_star-wars5.gif';
+import loader6 from '../loader/loader_star-wars6.gif';
 
 
 
@@ -11,7 +13,7 @@ export class LoaderStarWars extends Component {
   constructor(props) {
     super(props);
     
-    let loaders = [loader1, loader2, loader3, loader4];
+    let loaders = [loader1, loader2, loader3, loader4, loader5, loader6];
 
     this.loader = loaders[Math.floor(Math.random()*loaders.length)];
   }
