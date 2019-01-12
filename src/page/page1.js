0496755012fe2ef1e1ae274d9ffd8c26bf512a66
Mathieu <mathieu.25565@gmail.com>
2019-01-12 13:05:17 +0100

    add text editor on page 1

--- src/page/page1.js
+++ src/page/page1.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { TextEditor } from './TextEditor';
 
 export class Page1 extends Component {
   render() {
     return (
       <div>
-        <h1>Page 1</h1>
+        <h1>Éditeur de texte</h1>
+        <TextEditor/>
       </div>
     )
   }
 }
+
+
