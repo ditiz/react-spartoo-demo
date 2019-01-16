6bcf5b9b1fe40137136b71e39e6520d281f6ca34
Mathieu <mathieu.smarzyk@campus-numerique-in-the-alps.com>
2019-01-16 10:41:38 +0100

    add fliptable

---
+++ src/page/FlipTable.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './FlipTableCSS.css';
+
+export class FlipTable extends Component {
+    render() {
+        return (
+<div className="b">(<span className="b-arm">╯</span>°□°）<span className="b-arm">╯</span> <span className="b-table">┻━┻</span> <span className="b-table">┻━┻</span> <span className="b-table">┻━┻</span> <span className="b-table">┻━┻</span>
+</div>);
+    }
+}
--- src/page/page1.js
+++ src/page/page1.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { TextEditor } from './TextEditor';
+import { FlipTable } from './FlipTable';
 
 export class Page1 extends Component {
   render() {
     return (
       <div>
-        <h1>Éditeur de texte</h1>
+        <h1>Éditeur de texte pouet</h1>
         <TextEditor/>
+        <FlipTable/>
       </div>
     )
   }
